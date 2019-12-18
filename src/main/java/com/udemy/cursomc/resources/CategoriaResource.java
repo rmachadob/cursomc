0d591c7f3b5a7f9ccc70c309b00004481201b6d9
Ricardo Barbosa <ricardomachado2112@gmail.com>
2019-12-17 21:13:53 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
@@ -1,19 +1,33 @@
 package com.udemy.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //anotação com o ctrl shift O ele já acha e importa a dependência correspondente
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.cursomc.domain.Categoria;
 //essa classe será um controlador rest que vai responder por esse endpoint
 @RestController 
 @RequestMapping(value="/categorias")//nome do endpoint rest
 public class CategoriaResource {
 
 	//para ser uma função REST devo associar com algum dos verbos do HTTP
-	//como é uma requisição básica onde estou obtendo dados dados, uso o get
+	//como é uma requisição básica onde estou obtendo dados, uso o get
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Hurray! REST está funcionando!";
+	
+	public List<Categoria> listar() {
+		
+		Categoria cat1= new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
